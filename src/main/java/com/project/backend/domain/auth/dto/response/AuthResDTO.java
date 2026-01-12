11948f9be17042dffcb19255842bfca2857319ed
hyunseok <tjgustjr33@gmail.com>
2026-01-13 06:00:36 +0900

    ✨feat: 로그인 화면 리다이렉트 api, 인가 코드 받아서 사용자 정보 반환하는 callback api

---
+++ src/main/java/com/project/backend/domain/auth/dto/response/AuthResDTO.java
@@ -0,0 +1,35 @@
+package com.project.backend.domain.auth.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.project.backend.domain.auth.enums.Provider;
+import lombok.Builder;
+
+public class AuthResDTO {
+
+    @Builder
+    public record UserAuth(
+            Provider provider,
+            String providerId,
+            String email,
+            String name
+    ) {
+    }
+
+    @Builder
+    public record GoogleTokenRes(
+            @JsonProperty("access_token")
+            String accessToken,
+            @JsonProperty("expires_in")
+            Integer expiresIn,
+            @JsonProperty("refresh_token")
+            String refreshToken,
+            @JsonProperty("scope")
+            String scope,
+            @JsonProperty("token_type")
+            String tokenType,
+            @JsonProperty("id_token")
+            String idToken
+    ) {
+    }
+
+}
---
+++ src/main/java/com/project/backend/domain/auth/google/GoogleController.java
@@ -0,0 +1,34 @@
+package com.project.backend.domain.auth.google;
+
+import com.project.backend.global.apiPayload.CustomResponse;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+
+@Controller
+@RequiredArgsConstructor
+public class GoogleController {
+
+    private final GoogleAuthService googleAuthService;
+
+    @GetMapping("/api/v1/auth/google")
+    public void redirectToGoogle(HttpServletResponse response, HttpSession session) throws IOException {
+        String url = googleAuthService.generateAuthorizationUrl(session);
+        response.sendRedirect(url);
+    }
+
+    @GetMapping("/api/v1/auth/google/callback")
+    public CustomResponse<String> googleCallback(
+            @RequestParam("code") String code,
+            @RequestParam("state") String state,
+            HttpSession session
+    ) {
+        googleAuthService.exchangeCode(code, state, session);
+        return CustomResponse.onSuccess("Created", "로그인하셨습니다.");
+    }
+}
