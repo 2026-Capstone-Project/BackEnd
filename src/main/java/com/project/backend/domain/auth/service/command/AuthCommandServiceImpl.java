3794bf0ef30ffc4e6cb6abe2976b3989bbe139c7
Jimyeong Kim <kjm021221@icloud.com>
2026-01-13 18:43:32 +0900

    ♻️refactor: 쿠키 네이밍 변경

--- src/main/java/com/project/backend/domain/auth/service/command/AuthCommandServiceImpl.java
+++ src/main/java/com/project/backend/domain/auth/service/command/AuthCommandServiceImpl.java
@@ -9,16 +9,16 @@ import com.project.backend.domain.member.enums.Role;
 import com.project.backend.domain.member.service.MemberService;
 import com.project.backend.global.security.jwt.JwtUtil;
 import com.project.backend.global.security.userdetails.CustomUserDetails;
+import com.project.backend.global.security.utils.CookieUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
-
-import static com.project.backend.global.cookie.CookieUtils.createJwtCookies;
 
 @Slf4j
 @Service
@@ -26,9 +26,16 @@ import static com.project.backend.global.cookie.CookieUtils.createJwtCookies;
 @Transactional
 public class AuthCommandServiceImpl implements AuthCommandService {
 
+    @Value("${spring.jwt.access-expiration-time}")
+    private long accessExpMs;
+
+    @Value("${spring.jwt.refresh-expiration-time}")
+    private long refreshExpMs;
+
     private final AuthRepository authRepository;
     private final MemberService memberService;
     private final JwtUtil jwtUtil;
+    private final CookieUtil cookieUtil;
 
     public void loginOrSignup(HttpServletResponse response, AuthResDTO.UserAuth userAuth) {
 
@@ -40,8 +47,9 @@ public class AuthCommandServiceImpl implements AuthCommandService {
         String accessToken = jwtUtil.createJwtAccessToken(userDetails);
         String refreshToken = jwtUtil.createJwtRefreshToken(userDetails);
 
-        // TODO: 쿠키 생성하기
-//        createJwtCookies(response, "access-cookie", accessToken, 1000000L);
+        // 쿠키 생성하기
+        cookieUtil.createJwtCookie(response, "access_token", accessToken, accessExpMs);
+        cookieUtil.createJwtCookie(response, "refresh_token", refreshToken, refreshExpMs);
     }
 
     private Auth signup(AuthResDTO.UserAuth userAuth) {
