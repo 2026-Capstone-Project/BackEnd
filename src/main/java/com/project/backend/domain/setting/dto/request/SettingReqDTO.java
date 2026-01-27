6fd2f0de9825cb315a80d3d9b4dc12730d537712
Gyueon Lee <gon2yq@naver.com>
2026-01-28 02:07:14 +0900

    ♻️refactor: 패턴 애노테이션 메시지 변경

--- src/main/java/com/project/backend/domain/setting/dto/request/SettingReqDTO.java
+++ src/main/java/com/project/backend/domain/setting/dto/request/SettingReqDTO.java
@@ -10,7 +10,7 @@ public class SettingReqDTO {
             @NotBlank
             @Pattern(
                     regexp = "^([01]\\d|2[0-3]):00$",
-                    message = "HH:MM 형식, 00:00 ~ 23:00 범위의 정시"
+                    message = "HH:00 형식, 00:00 ~ 23:00 범위의 정시"
             )
             String dailyBriefingTime
     ) {
