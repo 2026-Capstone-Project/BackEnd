a72373ed811b5b9e7f83016b0ce2db58c152967e
Jimyeong Kim <kjm021221@icloud.com>
2026-02-03 16:09:18 +0900

    🔥Remove: 사용하지 않는 RecurringTodo 관련 코드 삭제
    레거시 코드인 RecurringTodo 관련 파일들을 삭제한다.
    
    Why:
    - RecurringTodo 엔티티는 현재 사용되지 않음
    - Todo 반복 기능은 TodoRecurrenceGroup + TodoRecurrenceException 방식으로 구현됨
    - RecurringTodoRepository가 어떤 서비스에서도 주입되지 않음
    - 불필요한 코드 제거로 유지보수성 향상
    
    삭제 파일:
    - RecurringTodo.java (엔티티)
    - RecurringTodoRepository.java (레포지토리)
    - RecurrenceType.java (Enum, RecurringTodo에서만 사용)

--- src/main/java/com/project/backend/domain/todo/repository/RecurringTodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.backend.domain.todo.repository;
-
-import com.project.backend.domain.todo.entity.RecurringTodo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecurringTodoRepository extends JpaRepository<RecurringTodo, Long> {
-}
