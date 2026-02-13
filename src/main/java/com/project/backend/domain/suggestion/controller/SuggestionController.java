88bb2686a054d55257e1b32e2b43b3af6b301790
Gyueon Lee <gon2yq@naver.com>
2026-02-13 21:45:41 +0900

    📝docs: SuggestionDocs 구현

--- src/main/java/com/project/backend/domain/suggestion/controller/SuggestionController.java
+++ src/main/java/com/project/backend/domain/suggestion/controller/SuggestionController.java
@@ -1,5 +1,6 @@
 package com.project.backend.domain.suggestion.controller;
 
+import com.project.backend.domain.suggestion.controller.docs.SuggestionDocs;
 import com.project.backend.domain.suggestion.dto.response.SuggestionResDTO;
 import com.project.backend.domain.suggestion.service.command.SuggestionCommandService;
 import com.project.backend.domain.suggestion.service.query.SuggestionQueryService;
@@ -17,11 +18,12 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/suggestions")
 @RequiredArgsConstructor
-public class SuggestionController {
+public class SuggestionController implements SuggestionDocs {
 
     private final SuggestionCommandService suggestionCommandService;
     private final SuggestionQueryService suggestionQueryService;
 
+    @Override
     @GetMapping()
     public CustomResponse<SuggestionResDTO.SuggestionListRes> getSuggestions(
             @AuthenticationPrincipal CustomUserDetails customUserDetails
@@ -31,6 +33,7 @@ public class SuggestionController {
         return CustomResponse.onSuccess("선제적 제안 목록 조회 성공", resDTO);
     }
 
+    @Override
     @PostMapping()
     public CustomResponse<String> createSuggestion(
             @AuthenticationPrincipal CustomUserDetails customUserDetails
@@ -71,6 +74,7 @@ public class SuggestionController {
 //        return CustomResponse.onSuccess("투두 반복 그룹에 대한 선제적 제안 생성 완료", null);
 //    }
 
+    @Override
     @PostMapping("/{suggestionId}/acceptance")
     public CustomResponse<String> acceptSuggestion(
             @AuthenticationPrincipal CustomUserDetails customUserDetails,
@@ -80,6 +84,7 @@ public class SuggestionController {
         return CustomResponse.onSuccess("선제적 제안 수락", null);
     }
 
+    @Override
     @PostMapping("{suggestionId}/rejection")
     public CustomResponse<String> rejectSuggestion(
             @AuthenticationPrincipal CustomUserDetails customUserDetails,
@@ -89,6 +94,7 @@ public class SuggestionController {
         return CustomResponse.onSuccess("선제적 제안 거절", null);
     }
 
+    @Override
     @DeleteMapping()
     public CustomResponse<String> deleteAllSuggestions(
             @AuthenticationPrincipal CustomUserDetails customUserDetails
