78a4827a6395638b55d73c02dc81e6c356dc91d3
Gyueon Lee <gon2yq@naver.com>
2026-04-06 00:18:26 +0900

    ✨feat: FriendRequest Entity 생성

---
+++ src/main/java/com/project/backend/domain/friend/repository/FriendRequestRepository.java
@@ -0,0 +1,9 @@
+package com.project.backend.domain.friend.repository;
+
+import com.project.backend.domain.friend.entity.FriendRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FriendRequestRepository extends JpaRepository<FriendRequest, Long> {
+}
